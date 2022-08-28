9420e6f8434053534d7c8acacd2442526ebfbdb4
Ben Jim <jimenezbenjamin@yahoo.com>
2022-08-28 20:55:35 +0000

    Add warning messages when the user is not logged in

---
+++ mynews/tests_forms.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from .forms import CommentForm, CreateReviewForm
+
+class TestCommentForm(TestCase):
+
+    def test_item_body_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+class CreateReviewForm(TestCase):
+
+    def test_item_body_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
